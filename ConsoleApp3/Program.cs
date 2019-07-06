a5ba5865b1863901517ff821de963b968b90488b
黄小华 <617662163@qq.com>
2019-07-06 13:26:04 +0800

    添加项目文件。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public class MyTest
+        {
+            public MyTest(string info)
+            {
+                Console.WriteLine(info);
+            }
+
+        }
+        public class BaseA
+        {
+            public static MyTest a1 = new MyTest("a1");
+            public MyTest a2 = new MyTest("a2");
+            static BaseA()
+            {
+                MyTest a3 = new MyTest("a3");
+            }
+            public BaseA()
+            {
+                MyTest a4 = new MyTest("a4");
+            }
+            public virtual void MyFun()
+            {
+                MyTest a5 = new MyTest("a5");
+            }
+        }
+        public class BaseB:BaseA
+        {
+            public static MyTest b1 = new MyTest("b1");
+            public MyTest b2 = new MyTest("b2");
+            static BaseB()
+            {
+                MyTest b3=new MyTest("b3"); 
+            }
+           public  BaseB()
+            {
+                MyTest b4 = new MyTest("b4");
+            }
+           public new void  MyFun()
+            {
+                MyTest b5= new MyTest("b5");
+            }
+        }
+        static void Main(string[] args)
+        {
+            BaseB baseB = new BaseB();
+            baseB.MyFun();
+        }
+    }
+}
